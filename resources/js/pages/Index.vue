fc8a3b42c6762f8bb828c47a60d4b2f4a02d3fbc
Kota Ogino <okitsu090807@gmail.com>
2019-10-30 22:37:16 +0900

    10月30日

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -1,102 +1,104 @@
 <template>
 <div class="container">
-    <div class="userLineGraph">
-        <line-chart :chart-data="datacollection" height="150"></line-chart>
-    </div>
-    <h2>User</h2>
-    <h3>今期間</h3>
-      <div v-for="(user,date,index) in data.originUser">
-        <p>{{date}}:{{user}}</p>
-      </div>
-    <h3>前期間</h3>
-      <div v-for="(user,date,index) in data.compareUser">
-        <p>{{date}}:{{user}}</p>
-      </div>
-    <h2 v-bind:style="styles">Summary</h2>
-    <p>セッション数：{{ data.session }}</p>
-    <p>前セッション数：{{ data.comSession }}</p>
-    <p>ページ/セッション：{{ data.pSession }}</p>
-    <p>前ページ/セッション：{{ data.comPSession }}</p>
-    <p>ユーザー数：{{ data.user }}</p>
-    <p>前ユーザー数：{{ data.comUser }}</p>
-    <p>滞在時間：{{ data.time }}</p>
-    <p>前滞在時間：{{ data.comTime }}</p>
-    <p>直帰率：{{ data.bRate }}</p>
-    <p>前直帰率：{{ data.comBRate }}</p>
-    <p>ページの価値：{{ data.value }}</p>
-    <p>前ページの価値：{{ data.comValue }}</p>
-    <p>CV：{{ data.cv }}</p>
-    <p>前CV：{{ data.comCv }}</p>
-    <p>ページの早さ：{{ data.speed }}</p>
-    <p>前ページの早さ：{{ data.comSpeed }}</p>
+  <div class="userLineGraph">
+    <line-chart :chart-data="datacollection" :options="options" :height="300" :width="740"></line-chart>
+  </div>
+
+  <h2 v-bind:style="styles">Summary</h2>
+  <p>セッション数：{{ data.session }}</p>
+  <p>前セッション数：{{ data.comSession }}</p>
+  <p>ページ/セッション：{{ data.pSession }}</p>
+  <p>前ページ/セッション：{{ data.comPSession }}</p>
+  <p>ユーザー数：{{ data.user }}</p>
+  <p>前ユーザー数：{{ data.comUser }}</p>
+  <p>滞在時間：{{ data.time }}</p>
+  <p>前滞在時間：{{ data.comTime }}</p>
+  <p>直帰率：{{ data.bRate }}</p>
+  <p>前直帰率：{{ data.comBRate }}</p>
+  <p>ページの価値：{{ data.value }}</p>
+  <p>前ページの価値：{{ data.comValue }}</p>
+  <p>CV：{{ data.cv }}</p>
+  <p>前CV：{{ data.comCv }}</p>
+  <p>ページの早さ：{{ data.speed }}</p>
+  <p>前ページの早さ：{{ data.comSpeed }}</p>
 </div>
 </template>
 <script>
 import LineChart from '../chart/LineChart.js'
 export default {
-    components: {
-        // ここで読んだコンポーネントをケバブケースにしたら普通に使えるっぽい
-        LineChart,
-        /* <line-chart></line-chart> */
-    },
-    data() {
-        return {
-            datacollection: {},
-            data: {},
-            styles: {
-                backgroundColor: 'green',
-                fontSize: '30px',
-                color: 'red',
-            },
-        }
-    },
-    mounted() {
-        axios.get('/api/analytics')
-            .then((res) => {
-                this.data = res.data,
-                    this.fillData()
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    },
-    methods: {
-        /**
-         * 入力されたデータの数に応じてランダムなチャートデータを作成する
-         */
-        fillData: function() {
-            var originUser = this.data['originUser'];
-            var compareUser = this.data['compareUser'];
-            var arrayLabel = [];
-            var arrayDataOne = [];
-            var arrayDataTwo = [];
-            for (var key in originUser) {
-              // console.log(key+':'+originUser[key]);
-              arrayLabel.push(key);
-              arrayDataOne.push(originUser[key]);
-            }
-            for (var key in compareUser) {
-              // console.log(key+':'+originUser[key]);
-              arrayDataTwo.push(compareUser[key]);
-            }
+  components: {
+    // ここで読んだコンポーネントをケバブケースにしたら普通に使えるっぽい
+    LineChart,
+    /* <line-chart></line-chart> */
+  },
+  data() {
+    return {
+      datacollection: {},
+      data: {},
+      styles: {
+        backgroundColor: 'green',
+        fontSize: '30px',
+        color: 'red',
+      },
+    }
+  },
+  mounted() {
+    axios.get('/api/analytics')
+      .then((res) => {
+          this.data = res.data,
+          this.fillData()
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  },
+  methods: {
+    /**
+     * 入力されたデータの数に応じてランダムなチャートデータを作成する
+     */
+    fillData: function() {
+      var originUser = this.data['originUser'];
+      var compareUser = this.data['compareUser'];
+      var arrayLabel = [];
+      var arrayDataOne = [];
+      var arrayDataTwo = [];
+      for (var key in originUser) {
+        // console.log(key+':'+originUser[key]);
+        arrayLabel.push(key);
+        arrayDataOne.push(originUser[key]);
+      }
+      for (var key in compareUser) {
+        // console.log(key+':'+originUser[key]);
+        arrayDataTwo.push(compareUser[key]);
+      }
 
-            this.datacollection = {
-                animation : true,
-                labels: arrayLabel,
-                datasets: [{
-                    label: 'Data One',
-                    borderColor: 'red',
-                    backgroundColor:'rgba(0, 0, 0, 0)',
-                    data: arrayDataOne
-                }, {
-                    label: 'Data Two',
-                    borderColor: 'blue',
-                    backgroundColor:'rgba(0, 0, 0, 0)',
-                    data: arrayDataTwo
-                }],
-
-            }
+      this.datacollection = {
+          animation: true,
+          labels: arrayLabel,
+          datasets: [{
+            label: '今期間',
+            borderColor: 'red',
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            data: arrayDataOne
+          }, {
+            label: '前期間',
+            borderColor: 'blue',
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            data: arrayDataTwo
+          }]
+        },
+        this.options = {
+          responsive:false,
+          scales: {
+            yAxes: [{
+              ticks: {
+                suggestedMin: 50,
+                suggestedMax: 100
+              }
+            }]
+          }
         }
     }
+  }
 }
 </script>
--- resources/js/pages/User.vue
+++ resources/js/pages/User.vue
@@ -21,38 +21,48 @@
         <div v-for="age in data.user[1]">
             <p>{{ age[0] }}:{{ age[1] }}</p>
         </div>
-    </div>
+</div>
     <div class="gender graphBox">
         <h2>Gender</h2>
-        <div v-for="(gender,index) in data.user[2]">
-            <p>{{ gender[0] }}:{{ gender[1] }}</p>
-        </div>
-        <div class="barGender">
-          <span class="barGenderMale" v-bind:style="{width:stylesMale}"></span>
-          <span class="barGenderFemale"  v-bind:style="{width:stylesFemale}"></span>
-        </div>
+        <DoughnutChartSex :chart-data="datacollectionSex" :options="optionsSex" :width="150" :height="150"></DoughnutChartSex>
     </div>
     <div class="device graphBox">
         <h2>Deivce</h2>
-        <div v-for="(device,index) in data.user[0]">
+        <!-- <div v-for="(device,index) in data.user[0]">
             <p>{{ device[0] }}:{{ device[1] }}</p>
             <span v-if="index===0" v-bind:style="{width:styleMax}" class="barDevice"></span>
             <span v-else v-bind:style="{width:stylesDevice[index]}" class="barDevice"></span>
-        </div>
+        </div> -->
+        <DoughnutChartDevice :chart-data="datacollectionDevice" :options="optionsDevice" :width="150" :height="150"></DoughnutChartDevice>
     </div>
     <div class="userType graphBox">
       <h2>UserType</h2>
-      <div v-for="(userType,index) in data.userTypes">
+      <!-- <div v-for="(userType,index) in data.userTypes">
         <p>{{userType[0][0]}}:{{userType[0][1]}}</p>
-      </div>
+      </div> -->
+      <DoughnutChartUser :chart-data="datacollectionUser" :options="optionsUser" :width="150" :height="150"></DoughnutChartUser>
     </div>
 </div>
 
 </template>
 <script>
+import DoughnutChartSex from '../chart/DoughnutChart.js'
+import DoughnutChartUser from '../chart/DoughnutChart.js'
+import DoughnutChartDevice from '../chart/DoughnutChart.js'
+
 export default {
+
+  components: {
+    // ここで読んだコンポーネントをケバブケースにしたら普通に使えるっぽい
+  DoughnutChartSex,
+  DoughnutChartUser,
+  DoughnutChartDevice,
+  },
     data() {
         return {
+            datacollectionSex: {},
+            datacollectionUser: {},
+            datacollectionDevice: {},
             data: {},
             styleMax: '100%',
             stylesCountry: {},
@@ -65,12 +75,12 @@ export default {
     created() {
         axios.get('/api/user')
             .then((res) => {
-                this.data = res.data,
+                    this.data = res.data,
                     this.widthCountry(),
                     this.widthCity(),
-                    this.widthDevice(),
-                    this.widthMale(),
-                    this.widthFemale()
+                    this.DoughnutChartDataSex(),
+                    this.DoughnutChartDataUser(),
+                    this.DoughnutChartDataDevice()
             })
             .catch(error => {
                 console.error(error);
@@ -104,30 +114,94 @@ export default {
             }
             this.stylesCity = w_arry;
         },
-        widthDevice: function() {
-            var maxNumber = this.data.user[0][0][1];
-            var w_arry = {};
-            for (var i = 1; i < 3; i++) {
-                var number = this.data.user[0][i][1];
-                var width = this.calRate(number,maxNumber);
-                w_arry[i] = width;
-            }
-            this.stylesDevice = w_arry;
-        },
-        widthMale: function() {
-            var maleNumber = this.data.user[2][0][1];
-            var femaleNumber = this.data.user[2][1][1];
-            var totalNumber = Number(maleNumber) + Number(femaleNumber);
-            var width = this.calRate(maleNumber,totalNumber);
-            this.stylesMale = width;
-        },
-      widthFemale: function() {
-        var maleNumber = this.data.user[2][0][1];
-        var femaleNumber = this.data.user[2][1][1];
-        var totalNumber = Number(maleNumber) + Number(femaleNumber);
-        var width = this.calRate(femaleNumber,totalNumber);
-        this.stylesFemale = width;
+    DoughnutChartDataSex:function(){
+      var maleNumber = this.data.user[2][0][1];
+      var femaleNumber = this.data.user[2][1][1];
+      var sexData = [];
+      sexData.push(maleNumber,femaleNumber);
+      this.datacollectionSex = {
+        // ラベル
+          labels: ["男性", "女性"],
+          // データ詳細
+          datasets: [{
+              data: sexData,
+              backgroundColor: [
+                 'blue',
+                 'red'
+              ]
+          }]
+      },
+      this.optionsSex = {
+        responsive:false,
+        cutoutPercentage: 80,
+        legend: {
+          position: 'bottom',
+          labels : {
+            fontSize: 12,
+          }
+        }
       }
+    },
+    DoughnutChartDataUser: function(){
+      var NewNumber = this.data.userTypes[0][0][1];
+      var ReturnNumber = this.data.userTypes[1][0][1];
+      var userData = [];
+      userData.push(NewNumber,ReturnNumber);
+      this.datacollectionUser = {
+        // ラベル
+          labels: ["新規ユーザー", "既存ユーザー"],
+          // データ詳細
+          datasets: [{
+              data: userData,
+              backgroundColor: [
+                 'blue',
+                 'skyblue'
+              ]
+          }]
+      },
+      this.optionsUser = {
+        responsive:false,
+        cutoutPercentage: 80,
+        legend: {
+          position: 'bottom',
+          // ★ [labels]を追加
+          labels : {
+            fontSize: 12,
+          }
+        }
+      }
+    },
+    DoughnutChartDataDevice: function(){
+      var mobileNumber = this.data.user[0][1][1];
+      var pcNumber = this.data.user[0][0][1];
+      var tabletNumber = this.data.user[0][2][1];
+      var deviceData = [];
+      deviceData.push(mobileNumber,pcNumber,tabletNumber);
+      this.datacollectionDevice = {
+        // ラベル
+          labels: ["モバイル", "PC", "タブレット"],
+          // データ詳細
+          datasets: [{
+              data: deviceData,
+              backgroundColor: [
+                 'red',
+                 'orange',
+                 'pink'
+              ]
+          }]
+      },
+      this.optionsDevice = {
+        responsive:false,
+        cutoutPercentage: 80,
+        legend: {
+          position: 'bottom',
+          // ★ [labels]を追加
+          labels : {
+            fontSize: 12,
+          }
+        }
+      }
+    }
     }
 }
 </script>
